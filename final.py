f30dd35004d24644a17569c60e01e58b50d83486
LizaChelishev <90146659+LizaChelishev@users.noreply.github.com>
2021-11-23 21:44:41 +0200

    DONE

--- final.py
+++ final.py
@@ -1,8 +1,16 @@
 def play_game():
     board = create_structure()
-    print_the_board = print_board(board)
+    print_board(board)
     player_1, player_2 = symbols_and_players()
-    full = board_full(board, player_1, player_2)
+    run_game(board, player_1, player_2)
+
+
+def number_of_rows(board):
+    return len(board)
+
+
+def number_of_columns(board):
+    return len(board[0])
 
 
 def create_structure():
@@ -17,135 +25,120 @@ def create_structure():
 def print_board(board):
     # This function prints the board
     rows = len(board)
-    cols = len(board)
-    print("    ")
+    cols = len(board[0])
+    print("")
     for r in range(rows):
-        print(board[r][0], " , ", board[r][1], " , ", board[r][2])
-        print("   ")
-    return board
+        for c in range(cols):
+            print(board[r][c], end='')
+            if c != len(board[0]) - 1:
+                print(" , ", end='')
+        print("\n")
 
 
 def symbols_and_players():
-    # This function decides the symbols of the players
-    player_1 = input("Player 1, do you want to be X or O? ")
-    if player_1 == "X":
-        player_2 = "O"
-        print("Player 2, you are O. ")
-    else:
-        player_2 = "X"
-        print("Player 2, you are X. ")
-    return [player_1, player_2]
+    player_1 = "X"
+    player_2 = "O"
+    return player_1, player_2
 
 
-def choice_invalid(row, column):
+def choice_invalid():
     # This function tells the player that their selection is invalid
-    print("Out of boarder. Pick another one. ")
-    return None
+    print("Out of board. Pick another one. ")
 
 
-def already_chosen(board, player_1, player_2, row, column):
+def already_chosen():
     # This function tells the player that their selection was already chosen
     print("The square you picked is already filled. Pick another one.")
-    return None
 
 
-def start_the_game(board, player_1, player_2, moves):
-    player = 0
+def get_current_player(moves, player_1, player_2):
     if moves % 2 == 0:
         player = player_2
     elif moves % 2 == 1:
         player = player_1
-    print("Player " + player + ", it is your turn. ")
-    row = int(input("Pick a row:"
-                    "[upper row: enter 0, middle row: enter 1, bottom row: enter 2]:"))
-    column = int(input("Pick a column:"
-                       "[left column: enter 0, middle column: enter 1, right column enter 2]"))
+    return player
 
-    # Check if players' selection is valid
-    while (row > 2 or row < 0) or (column > 2 or column < 0):
-        choice_invalid(row, column)
+
+def is_users_input_valid(board, row, column):
+    is_valid = False
+    if (number_of_rows(board) - 1 >= row >= 0) and (number_of_columns(board) - 1 >= column >= 0):
+        if board[row][column] == '_':
+            is_valid = True
+        else:
+            already_chosen()
+    else:
+        choice_invalid()
+    return is_valid
+
+
+def start_the_game(board, player):
+    print("Player " + player + ", it is your turn.")
+
+    # Check if the players choice is valid
+    while True:
         row = int(input("Pick a row[upper row:"
                         "[enter 0, middle row: enter 1, bottom row: enter 2]:"))
         column = int(input("Pick a column:"
                            "[left column: enter 0, middle column: enter 1, right column enter 2]"))
-
-    # Check if the place you chose is already filled
-    while (board[row][column] == player_1) or (board[row][column] == player_2):
-        filled = already_chosen(board, player_1, player_2, row, column)
-        row = int(input("Pick a row[upper row:"
-                        "[enter 0, middle row: enter 1, bottom row: enter 2]:"))
-        column = int(input("Pick a column:"
-                           "[left column: enter 0, middle column: enter 1, right column enter 2]"))
+        if is_users_input_valid(board, row, column) is True:
+            break
 
     # Locates player's symbol on the board
-    if player == player_1:
-        board[row][column] = player_1
-
-    else:
-        board[row][column] = player_2
+    board[row][column] = player
 
     return board
 
 
-def board_full(board, player_1, player_2):
+def run_game(board, player_1, player_2):
     moves = 1
-    winner = True
-    # This function check if the board is full
-    while moves < 10 and winner == True:
-        start_game = start_the_game(board, player_1, player_2, moves)
-        print_the_board = print_board(board)
+    winner = False
+    number_of_board_positions = number_of_rows(board) * number_of_columns(board)
+    while moves < number_of_board_positions + 1 and winner is False:
+        player = get_current_player(moves, player_1, player_2)
+        start_the_game(board, player)
+        print_board(board)
 
-        if moves == 9:
+        if moves == number_of_board_positions:
             print("The board is full. Game over.")
-            if winner is True:
+            if winner is False:
                 print("Draw game. Start over.")
 
-        winner = the_winner(board, player_1, player_2)
+        winner = the_winner(board, player_1)
+        if winner is False:
+            winner = the_winner(board, player_2)
+
         moves += 1
-    if winner is False:
+    if winner is True:
         print("Game over.")
-    return None
 
 
-def the_winner(board, player_1, player_2):
+def the_winner(board, player):
     # This function checks if any winner is winning
-    winner = True
+    winner = False
     # Check the rows
-    for row in range(0, 3):
-        if board[row][0] == board[row][1] == board[row][2] == player_1:
-            winner = False
-            print("Player " + player_1 + ", you won!")
-
-        elif board[row][0] == board[row][1] == board[row][2] == player_2:
-            winner = False
-            print("Player " + player_2 + ", you won!")
+    for row in range(0, len(board)):
+        if board[row][0] == player and board[row][1] == player and board[row][2] == player:
+            winner = True
+            print("Player " + player + ", you won!")
+            return winner
 
     # Check the columns
-    for col in range(0, 3):
-        if board[0][col] == board[1][col] == board[2][col] == player_1:
-            winner = False
-            print("Player " + player_1 + ", you won!")
-        elif board[0][col] == board[1][col] == board[2][col] == player_2:
-            winner = False
-            print("Player " + player_2 + ", you won!")
+    for col in range(0, len(board[0])):
+        if board[0][col] == player and board[1][col] == player and board[2][col] == player:
+            winner = True
+            print("Player " + player + ", you won!")
+            return winner
 
     # Check the diagonals
-    if board[0][0] == board[1][1] == board[2][2] == player_1:
-        winner = False
-        print("Player " + player_1 + ", you won!")
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        winner = True
+        print("Player " + player + ", you won!")
 
-    elif board[0][0] == board[1][1] == board[2][2] == player_2:
-        winner = False
-        print("Player " + player_2 + ", you won!")
-
-    elif board[0][2] == board[1][1] == board[2][0] == player_1:
-        winner = False
-        print("Player " + player_1 + ", you won!")
-
-    elif board[0][2] == board[1][1] == board[2][0] == player_2:
-        winner = False
-        print("Player " + player_2 + ", you won!")
+    elif board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        winner = True
+        print("Player " + player + ", you won!")
 
     return winner
+
 
 play_game()
